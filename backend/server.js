a331b25d712aea28f372864e202d4aad916359b6
abuthahir-rasick <rasick908@gmail.com>
2026-02-11 18:49:20 +0530

    add code

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ const mongoose=require('mongoose');
 const authRoutes=require('./routes/authRoutes');
 const employeeRoutes=require('./routes/employeeRoutes');
 const managerRoutes=require('./routes/managerRoutes');
+require('dotenv').config(); 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/task-assigner')
+ mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("DB Connected"))
 .catch((err)=>console.log(err));
 
@@ -17,7 +18,7 @@ app.use('/auth',authRoutes);
 app.use('/employee',employeeRoutes);
 app.use('/manager',managerRoutes);
 
-const port=5000;
+const port=process.env.PORT;
 app.listen(port,()=>{
     console.log(`Server is Listening on ${port}`)
 })
